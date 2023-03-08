bc67bd87c6d41dd1fa7dd51af8a6950108b5cd62
Marcin Popko <marcinpopko@outlook.com>
2023-03-08 19:20:57 +0100

    initial commit
    db and secret password i gitignored pw.json file, added pw_example.json

---
+++ tablechart/chart_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class chart_app_config(AppConfig):
+    name = 'chart_app'
---
+++ tablechart/chart_app/views.py
@@ -0,0 +1,31 @@
+from django.views import View
+from django.shortcuts import render
+from django.db import connection
+import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import plotly.express as px
+import io
+import urllib, base64
+import pandas as pd
+
+# Define the SQL query to fetch data from the database
+def myview(request):
+    now = datetime.datetime.now()
+    last_24h = now - datetime.timedelta(hours=24)
+
+    with connection.cursor() as cursor:
+        sql_query = f"SELECT date, sport, family, small, ice FROM poolStats WHERE date >= '{last_24h}' ORDER BY `poolStats`.`date` ASC"
+        cursor.execute(sql_query)
+        data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=['date', 'sport', 'family', 'small', 'ice'])
+    date = df['date']
+    sport = df['sport']
+    family = df['family']
+    small = df['small']
+    ice = df['ice'] 
+    fig = px.line(df, x=date, y=[sport, family, small, ice])
+    #fig.update_traces(name=['Sportowa', 'Rodzinna', 'Kameralna', 'Lodowisko'])
+
+    last_date = df['date'].iloc[-1].strftime('%d.%m.%Y %H:%M')
+    return render(request, 'chart.html', {'plot': fig.to_html(full_html=False), 'date': last_date, 'sport' : sport.iloc[-1], 'family' : family.iloc[-1], 'small': small.iloc[-1], "ice" : ice.iloc[-1]})
