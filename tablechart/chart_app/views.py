f08467378cf5d27759c1216ab05a3982c44b882c
Marcin Popko <marcinpopko@outlook.com>
2023-03-09 12:14:06 +0100

    mobile optimization

--- tablechart/chart_app/views.py
+++ tablechart/chart_app/views.py
@@ -12,7 +12,7 @@ import pandas as pd
 # Define the SQL query to fetch data from the database
 def myview(request):
     now = datetime.datetime.now()
-    last_24h = now - datetime.timedelta(hours=24)
+    last_24h = now - datetime.timedelta(hours=12)
 
     with connection.cursor() as cursor:
         sql_query = f"SELECT date, sport, family, small, ice FROM poolStats WHERE date >= '{last_24h}' ORDER BY `poolStats`.`date` ASC"
@@ -29,7 +29,17 @@ def myview(request):
     fig.update_traces(name='Rodzinna', selector=dict(name='family'))
     fig.update_traces(name='Kameralna', selector=dict(name='small'))
     fig.update_traces(name='Lodowisko', selector=dict(name='ice'))
-    fig.update_layout(title="Stan zajętości obiektów BOSiR (ostatnie 24h)", xaxis_title="Czas", yaxis_title="Liczba osób")
+    fig.update_layout(title="Stan zajętości obiektów BOSiR (ostatnie 12h)", xaxis_title="Czas", yaxis_title="Liczba osób")
+    fig.update_layout(
+        legend=dict(
+            orientation='h', # Set the orientation to horizontal
+            yanchor='top', # Anchor the legend to the top of the chart
+            y=-0.5, # Adjust the position of the legend to be below the chart
+            xanchor='right', # Anchor the legend to the right of the chart
+            x=1 # Adjust the position of the legend
+        ),
+        #margin=dict(t=50, b=0) # Adjust the top and bottom margins
+    )
     #fig.update_traces(text=["Sportowa", "Rodzinna", "Kameralna", "Lodowisko"])
 
     last_date = df['date'].iloc[-1].strftime('%d.%m.%Y %H:%M')
