d377875a9e7fc5d9397999c32acc60822cfa9aae
Marcin Popko <marcinpopko@outlook.com>
2023-03-09 15:09:01 +0100

    bootstrap template example

--- tablechart/chart_app/views.py
+++ tablechart/chart_app/views.py
@@ -2,15 +2,11 @@ from django.views import View
 from django.shortcuts import render
 from django.db import connection
 import datetime
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 import plotly.express as px
-import io
-import urllib, base64
 import pandas as pd
 
 # Define the SQL query to fetch data from the database
-def myview(request):
+def myview2(request):
     now = datetime.datetime.now()
     last_24h = now - datetime.timedelta(hours=12)
 
@@ -44,3 +40,22 @@ def myview(request):
 
     last_date = df['date'].iloc[-1].strftime('%d.%m.%Y %H:%M')
     return render(request, 'chart.html', {'plot': fig.to_html(full_html=False), 'date': last_date, 'sport' : sport.iloc[-1], 'family' : family.iloc[-1], 'small': small.iloc[-1], "ice" : ice.iloc[-1]})
+
+
+def myview(request):
+    now = datetime.datetime.now()
+    last_24h = now - datetime.timedelta(hours=12)
+
+    with connection.cursor() as cursor:
+        sql_query = f"SELECT date, sport, family, small, ice FROM poolStats WHERE date >= '{last_24h}' ORDER BY `poolStats`.`date` ASC"
+        cursor.execute(sql_query)
+        data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=['date', 'sport', 'family', 'small', 'ice'])
+    date = df['date']
+    sport = df['sport']
+    family = df['family']
+    small = df['small']
+
+    last_date = df['date'].iloc[-1].strftime('%d.%m.%Y %H:%M')
+    return render(request, 'index.html', {'date': list(date.dt.strftime('%Y-%m-%d %H:%M')), 'sport' : list(sport), 'family' : list(family), 'small': list(small),
+                                          'lastdate': last_date, 'lastsport' : sport.iloc[-1], 'lastfamily' : family.iloc[-1], 'lastsmall': small.iloc[-1]})
--- tablechart/tablechart/settings.py
+++ tablechart/tablechart/settings.py
@@ -58,7 +58,6 @@ ROOT_URLCONF = 'tablechart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
