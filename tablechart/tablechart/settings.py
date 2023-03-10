bf5e46d8ca1fce085fe0ee4d40f69e2adb29dad3
Marcin Popko <marcinpopko@outlook.com>
2023-03-10 18:08:54 +0100

    fix

--- tablechart/tablechart/settings.py
+++ tablechart/tablechart/settings.py
@@ -66,6 +66,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'mime_types': {
+                'css': 'text/css',
+            }
         },
     },
 ]
