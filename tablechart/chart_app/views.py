ba93a5def84d608013efc6b559e64aed961de93f
Marcin Popko <marcinpopko@outlook.com>
2023-03-18 11:36:51 +0100

    stats view prototype

--- tablechart/chart_app/views.py
+++ tablechart/chart_app/views.py
@@ -5,8 +5,10 @@ import datetime
 import plotly.express as px
 import pandas as pd
 from django.http import JsonResponse
+from django.template.loader import render_to_string
 
 def content_view(request):
+    stats_chart = stats_view(request)
     now = datetime.datetime.now()
     today = datetime.datetime(now.year, now.month, now.day, 6)
   
@@ -33,9 +35,39 @@ def content_view(request):
     family_percent = round((last_family/150)*100)
     small_percent = round((last_small/30)*100)
     last_date = df['date'].iloc[-1].strftime('%d.%m.%Y %H:%M')
-    return render(request, 'content.html', {'date': list(date.dt.strftime('%Y-%m-%d %H:%M')), 'sport' : list(sport), 'family' : list(family), 'small': list(small),
+    return render(request, 'content.html', {'stats_chart': stats_chart, 'date': list(date.dt.strftime('%Y-%m-%d %H:%M')), 'sport' : list(sport), 'family' : list(family), 'small': list(small),
                                           'lastdate': last_date, 'lastsport' : last_sport, 'lastfamily' : last_family, 'lastsmall': last_small,
                                           'sport_percent': sport_percent, 'family_percent': family_percent, 'small_percent': small_percent})
+
+def stats_view(request):
+    with connection.cursor() as cursor:
+        sql_query = f"SELECT date, sport, family, small FROM poolStats ORDER BY `poolStats`.`date` ASC"
+        cursor.execute(sql_query)
+        data = cursor.fetchall()
+    
+    if len(data) == 0:
+        stats_chart = render_to_string('stats_chart.html', {'date_stat': 0, 'sport_stat' : 0, 'family_stat' : 0, 'small_stat': 0})
+        return stats_chart
+    
+    df = pd.DataFrame(data, columns=['date', 'sport', 'family', 'small'])
+    df['date'] = pd.to_datetime(df['date'])
+    weekday_names = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
+    # Map the day of the week integers to weekday names and group the data by weekday and time separately
+    df_weekly_mean = df.groupby([df["date"].dt.dayofweek.map(weekday_names), df["date"].dt.time])["sport", "family", "small"].mean().round()
+    df_sunday = df_weekly_mean.loc["Sunday"]
+    df_sunday.index = pd.to_datetime(df_sunday.index.map(lambda t: datetime.datetime.combine(datetime.datetime.today(), t).strftime('%H:%M')))
+    df_sunday = df_sunday.between_time("6:00", "22:00")
+
+    time_sunday = df_sunday.index.tolist()
+    time_sunday_formatted = [dt.strftime("%H:%M") for dt in time_sunday]
+    df_sunday['time'] = time_sunday_formatted
+    sport = df_sunday['sport']
+    family = df_sunday['family']
+    small = df_sunday['small']
+    stats_chart = render_to_string('stats_chart.html', {'date_stat': time_sunday_formatted, 'sport_stat' : list(sport), 'family_stat' : list(family), 'small_stat': list(small)})
+    return stats_chart
+
+
 
 def update_chart(request):
     now = datetime.datetime.now()
