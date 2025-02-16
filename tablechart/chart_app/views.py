b23710f54c405fb53f9e5df7e8913d22ee63ee49
Marcin Popko <marcinpopko@outlook.com>
2025-02-16 08:12:16 +0100

    Django version bump

--- tablechart/chart_app/views.py
+++ tablechart/chart_app/views.py
@@ -11,7 +11,7 @@ from django.core.cache import cache
 import time
 import pytz
 
-ver_num = "0.1.3"
+ver_num = "0.1.4"
 
 def content_view(request):
     #TODO do one sql query and fetch data to live view
